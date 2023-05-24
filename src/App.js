19ee967549e5701c3a4acdcecf7a5cfb1c79b9e0
Alena Mashukova <alenamashukova@Alenas-MacBook-Pro.local>
2023-05-24 13:24:10 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
+// https://dev.to/franciscomendes10866/react-form-validation-with-react-hook-form-and-yup-4a98
+
 import './App.css';
+import {useForm} from 'react-hook-form';
+import * as yup from 'yup';
+import {yupResolver} from '@hookform/resolvers/yup';
 
 function App() {
+
+  const yupSchema = yup.object().shape({
+    email: yup.string().email().required(),
+    password: yup.string().min(8).max(32).required(),
+  })
+
+  const {register, reset, handleSubmit, formState: {errors}} = useForm({
+    resolver: yupResolver(yupSchema),
+  })
+
+  const onSubmitHandle = (data) => {
+    console.log(data);
+    reset();
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit(onSubmitHandle)}>
+        <input 
+        {...register('email')}
+          placeholder='type email'
+          type='email'
+          required
+        />
+        <p>{errors.email?.message}</p>
+        <br/>
+        <input
+        {...register('password')}
+          placeholder='type password'
+          type='password'
+          required/>
+          <p>{errors.password?.message}</p>
+          <br />
+        <button type='submit'>Submit</button>
+
+      </form>
     </div>
   );
 }
